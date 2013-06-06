73371c8a8880f67e42565f53e83ea4ec0ecb9fe4
Julien Lafont <jla@zenexity.com>
2013-06-06 20:02:29 +0200

    Add Scalariform to build

--- mailer/project/Build.scala
+++ mailer/project/Build.scala
@@ -1,17 +1,19 @@
 import sbt._
 import Keys._
 
-object MinimalBuild extends Build {
-  
+import com.typesafe.sbt.SbtScalariform._
+import scalariform.formatter.preferences._
 
-  lazy val buildVersion =  "2.1.0"
-  lazy val playVersion =  "2.1.0"
-  
+object MinimalBuild extends Build {
+
+  lazy val buildVersion = "2.1.1"
+  lazy val playVersion = "2.1.1"
+
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
   
-  lazy val root = Project(id = "play-plugins-mailer", base = file("."), settings = Project.defaultSettings).settings(
+  lazy val root = Project(id = "play-plugins-mailer", base = file("."), settings = Project.defaultSettings ++ defaultScalariformSettings).settings(
     version := buildVersion,
     scalaVersion := "2.10.0",
     publishTo <<= (version) { version: String =>
@@ -19,12 +21,12 @@ object MinimalBuild extends Build {
                 if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "maven-snapshots/")
                 else                                   Some("releases"  at nexus + "maven-releases/")
     },
-    organization := "com.typesafe",
+    organization := "fr.studio-dev",
     resolvers += repo,
     javacOptions ++= Seq("-source","1.6","-target","1.6", "-encoding", "UTF-8"),
     javacOptions += "-Xlint:unchecked",
     libraryDependencies += "play" %% "play" % playVersion % "provided",
-    libraryDependencies += "org.apache.commons" % "commons-email" % "1.2",
-    libraryDependencies += "com.typesafe" %% "play-plugins-util" % buildVersion
+    libraryDependencies += "org.apache.commons" % "commons-email" % "1.2"
   )
 }
+
---
+++ mailer/project/plugins.sbt
@@ -0,0 +1,2 @@
+// Scalariform
+addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.0.1")
